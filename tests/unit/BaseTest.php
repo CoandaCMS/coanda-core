707a687d4e5b7286b6f42058ebface54d815fad6
Alex Dover <me@alexdover.co.uk>
2014-04-22 18:19:33 +0100

    Removed use of orchestra/testbench - also added illuminate/database. Wanted to try and make the unit tests true unit tests.

--- tests/unit/BaseTest.php
+++ tests/unit/BaseTest.php
@@ -1,22 +1,10 @@
 <?php
 
-abstract class BaseTest extends \Orchestra\Testbench\TestCase {
+abstract class BaseTest extends \PHPUnit_Framework_TestCase {
 
 	public function setUp()
 	{
 	    parent::setUp();
-	}
-
-	protected function getPackageProviders()
-	{
-	    return array('CoandaCMS\Coanda\CoandaServiceProvider');
-	}
-
-	protected function getPackageAliases()
-	{
-		return array(
-		    'Coanda' => 'CoandaCMS\Coanda\Facades\Coanda'
-		);
 	}
 
 	public function tearDown()
