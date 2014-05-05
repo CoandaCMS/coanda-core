b0f2acc2dac818acb4be1bbdd9dbf5a45d821e19
Alex Dover <me@alexdover.co.uk>
2014-05-05 21:38:52 +0100

    Missing from previous commit!

--- src/views/admin/modules/layout/index.blade.php
+++ src/views/admin/modules/layout/index.blade.php
@@ -30,10 +30,11 @@
 
 		<div class="page-tabs">
 			<ul class="nav nav-tabs">
-				<li class="active"><a href="#pages" data-toggle="tab">Layouts</a></li>
+				<li @if (!Input::has('page')) class="active" @endif><a href="#layouts" data-toggle="tab">Layouts</a></li>
+				<li @if (Input::has('page')) class="active" @endif><a href="#blocks" data-toggle="tab">All blocks</a></li>
 			</ul>
 			<div class="tab-content">
-				<div class="tab-pane active" id="subpages">
+				<div class="tab-pane @if (!Input::has('page')) active @endif" id="layouts">
 
 					{{ Form::open(['url' => Coanda::adminUrl('pages')]) }}
 
@@ -65,6 +66,30 @@
 
 					{{ Form::close() }}
 				</div>
+
+				<div class="tab-pane @if (Input::has('page')) active @endif" id="blocks">
+
+					@if ($block_list->count() > 0)
+						<table class="table table-striped">
+							@foreach ($block_list as $block)
+								<tr class="status-{{ $block->status }}">
+									<td>
+										<i class="fa fa-th-large"></i>
+										<a href="{{ Coanda::adminUrl('layout/block-view/' . $block->id) }}">{{ $block->present()->name }}</a>
+									</td>
+									<td>{{ $block->blockType()->name() }}</td>
+									<td class="tight"><a href="{{ Coanda::adminUrl('layout/block-edit/' . $block->id) }}"><i class="fa fa-pencil-square-o"></i></a></td>
+								</tr>
+							@endforeach
+						</table>
+
+						{{ $block_list->links() }}
+					@else
+						<p>No layout blocks have been created.</p>
+					@endif
+
+				</div>
+
 			</div>
 		</div>
 	</div>
