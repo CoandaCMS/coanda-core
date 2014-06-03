6fc17027a0b0d1a3268b0d94fa45fc8609393be1
Alex Dover <me@alexdover.co.uk>
2014-06-03 20:49:00 +0100

    Added an optional identifier to the exception.

--- src/CoandaCMS/Coanda/Exceptions/ValidationException.php
+++ src/CoandaCMS/Coanda/Exceptions/ValidationException.php
@@ -11,12 +11,15 @@ class ValidationException extends \Exception {
      */
     private $invalid_fields = [];
 
+    private $invalid_identifier;
+
     /**
      * @param string $invalid_fields
      */
-    public function __construct($invalid_fields)
+    public function __construct($invalid_fields, $invalid_identifier = false)
 	{
 		$this->invalid_fields = $invalid_fields;
+        $this->invalid_identifier = $invalid_identifier;
 	}
 
     /**
@@ -27,4 +30,11 @@ class ValidationException extends \Exception {
 		return $this->invalid_fields;
 	}
 
+    /**
+     * @return array|string
+     */
+    public function getInvalidIdentifier()
+    {
+        return $this->invalid_identifier;
+    }
 }
