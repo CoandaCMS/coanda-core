ea97f3c6b5294518582bf69953bcdb34b17adad6
Alex Dover <me@alexdover.co.uk>
2014-07-03 16:23:28 +0100

    Add the extra/new/missing attributes to the attributes array. Does the same as the PageLocation does, ideally this needs to be centralised somewhere.

--- src/CoandaCMS/Coanda/Pages/PagesModuleProvider.php
+++ src/CoandaCMS/Coanda/Pages/PagesModuleProvider.php
@@ -453,7 +453,19 @@ class PagesModuleProvider implements \CoandaCMS\Coanda\CoandaModuleProvider {
 		{
 			$attributes->{$attribute->identifier} = $attribute->render($page, $pagelocation);
 		}
- 
+
+		// Add any attributes which are on the definition, but not in the object..
+		$pageType = $page->pageType();
+		$attribute_definition_list = $pageType->attributes();
+
+		foreach ($attribute_definition_list as $attribute_definition_identfier => $attribute_definition)
+		{
+			if (!property_exists($attributes, $attribute_definition_identfier))
+			{
+				$attributes->{$attribute_definition_identfier} = '';
+			}
+		}
+
         return $attributes;
 	}
 
