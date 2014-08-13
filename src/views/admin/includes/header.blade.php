21de4691103a8fbf6506e0992eddbfa8feefe72b
Alex Dover <me@alexdover.co.uk>
2014-08-13 10:25:50 +0100

    Further work on the header, including a modal to confirm you wish to log out.

--- src/views/admin/includes/header.blade.php
+++ src/views/admin/includes/header.blade.php
@@ -36,13 +36,13 @@
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
 				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Coanda::currentUser()->first_name }} <span class="caret"></span></a>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{ Coanda::currentUser()->first_name }} <span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
-						<li><a href="#">Profile</a></li>
-						<li><a href="#">Password</a></li>
+						<li><a href="#">My Account</a></li>
 						<li><a href="#">My Drafts</a></li>
+						<li><a href="#">Change Password</a></li>
 						<li class="divider"></li>
-						<li><a href="{{ Coanda::adminUrl('logout') }}">Log out</a></li>
+						<li><a href="#" data-toggle="modal" data-target=".bs-example-modal-sm"><i class="fa fa-power-off"></i> Log out</a></li>
 					</ul>
 				</li>
 			</ul>
@@ -53,5 +53,23 @@
 
 			<div class="clearfix"></div>		
 		</div>
-      </div>
-    </div>
+	</div>
+</div>
+
+<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">
+					<span aria-hidden="true">×</span>
+					<span class="sr-only">Close</span>
+				</button>
+				<p class="modal-title">Are you sure?</p>
+			</div>
+			<div class="modal-body">
+				<a href="{{ Coanda::adminUrl('logout') }}" class="btn btn-primary">Yes, please log me out</a>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+			</div>
+		</div>
+	</div>
+</div>
