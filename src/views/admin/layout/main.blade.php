23c28c5d9e8487ac335300d667fab71f21a4dc98
Alex Dover <me@alexdover.co.uk>
2014-10-13 15:37:26 +0100

    Added missing responsive meta tags.

--- src/views/admin/layout/main.blade.php
+++ src/views/admin/layout/main.blade.php
@@ -4,6 +4,9 @@
 		<title>@yield('page_title') | Coanda CMS</title>
 		<meta charset="utf-8">
 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" />
 		<link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' type="text/css">
