3d8f4623d36048e21e4053b5b80b3717e38958aa
Alex Dover <me@alexdover.co.uk>
2014-10-27 12:58:10 +0000

    Parent page is conditional on it not being null, so by adding ->in(null) to a query you can query the entire page tree.

--- src/CoandaCMS/Coanda/Pages/Repositories/Eloquent/Queries/SubPageQuery.php
+++ src/CoandaCMS/Coanda/Pages/Repositories/Eloquent/Queries/SubPageQuery.php
@@ -98,9 +98,13 @@ class SubPageQuery {
 
 		$query = $page_model
                     ->select('pages.*')
-					->where('parent_page_id', $this->parent_page_id)
 					->where('is_trashed', '=', '0')
 					->where('is_home', '=', '0');
+
+        if ($this->parent_page_id !== null)
+        {
+            $query->where('parent_page_id', $this->parent_page_id);
+        }
 
 		$query->join('pageversions', 'pages.id', '=', 'pageversions.page_id');
 		$query->where('pageversions.version', '=', \DB::raw('pages.current_version'));
