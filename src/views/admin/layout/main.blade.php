60a7635c9e8618404287e6546863fe37f1003030
Alex Dover <me@alexdover.co.uk>
2014-11-04 09:34:05 +0000

    Updated to font awesome 4.2
    Updated main admin layout to use latest font awesome css

--- src/views/admin/layout/main.blade.php
+++ src/views/admin/layout/main.blade.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" />
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" />
 		<link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' type="text/css">
 		<link rel="stylesheet" href="{{ asset('packages/coandacms/coanda-core/summernote/summernote.css') }}">
 		<link rel="stylesheet" href="{{ asset('packages/coandacms/coanda-core/datepicker/bootstrap-datetimepicker.min.css') }}">
