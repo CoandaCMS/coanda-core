93c055d096149d6ebd90961a034f96837feb8355
Alex Dover <me@alexdover.co.uk>
2014-11-04 09:49:06 +0000

    Fixed incorrect formatting of the date when editing.

--- src/views/admin/core/attributes/edit/date.blade.php
+++ src/views/admin/core/attributes/edit/date.blade.php
@@ -9,7 +9,7 @@ $prefill_data
 	<label class="control-label" for="attribute_{{ $attribute_identifier }}">{{ $attribute_name }} @if ($is_required) * @endif</label>
 
 	<div class="input-group datetimepicker" data-date-format="DD/MM/YYYY" data-hide-time="true">
-		<input type="text" class="date-field form-control" id="attribute_{{ $attribute_identifier }}_date" name="attributes[{{ $attribute_identifier }}]" value="{{ ($old_input ? $old_input : (isset($prefill_data) ? $prefill_data : '')) }}">
+		<input type="text" class="date-field form-control" id="attribute_{{ $attribute_identifier }}_date" name="attributes[{{ $attribute_identifier }}]" value="{{ ($old_input ? $old_input : ((isset($prefill_data) && is_object($prefill_data)) ? $prefill_data->format(Config::get('coanda::coanda.date_format')) : '')) }}">
 		<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
 	</div>
 
