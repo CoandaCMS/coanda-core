3235ab6981f3e1e643311f3c73fc44c169728ccd
Alex Dover <me@alexdover.co.uk>
2014-12-02 10:12:43 +0000

    Added the ability to remove the image which has been uploaded.

--- src/views/admin/core/attributes/edit/image.blade.php
+++ src/views/admin/core/attributes/edit/image.blade.php
@@ -9,10 +9,11 @@ $prefill_data
 	<label class="control-label" for="attribute_{{ $attribute_identifier }}_file">{{ $attribute_name }} @if ($is_required) * @endif</label>
 
 	@if ($prefill_data)
-		<div>
+		<div class="image-preview" style="margin: 0 0 10px 0;" id="image_preview_{{ $attribute_identifier }}">
 			<img src="{{ url($prefill_data->resizeUrl(150)) }}" width="150" class="img-thumbnail">
-			<input type="hidden" name="attributes[{{ $attribute_identifier }}][media_id]" value="{{ $prefill_data->id }}">
+			<input type="hidden" id="existing_image_id_{{ $attribute_identifier }}" name="attributes[{{ $attribute_identifier }}][media_id]" value="{{ $prefill_data->id }}">
 		</div>
+		<a href="#" style="margin: 0 0 10px 0;" name="remove_image" id="remove_image_{{ $attribute_identifier }}" class="btn btn-danger btn-sm">Remove</a>
 	@endif
 
 	<input type="file" name="attribute_{{ $attribute_identifier }}_file" id="attribute_{{ $attribute_identifier }}_file">
@@ -22,3 +23,18 @@ $prefill_data
     @endif
 
 </div>
+
+@section('footer')
+<script type="text/javascript">
+$('#remove_image_{{ $attribute_identifier }}').on('click', function () {
+
+	$('#existing_image_id_{{ $attribute_identifier }}').attr('value', 0);
+	$('#image_preview_{{ $attribute_identifier }}').remove();
+
+	$(this).remove();
+
+	return false;
+
+});
+</script>
+@append
