c89dc73944cb7e8c8ab2150809078debc8a67647
Alex Dover <me@alexdover.co.uk>
2014-12-15 10:59:59 +0000

    Added in a default (very basic) template for the missing page type.

--- src/CoandaCMS/Coanda/Pages/PageTypes/MissingPageType.php
+++ src/CoandaCMS/Coanda/Pages/PageTypes/MissingPageType.php
@@ -2,12 +2,33 @@
 
 class MissingPageType extends \CoandaCMS\Coanda\Pages\PageType {
 
+    /**
+     * @var string
+     */
     protected $name = 'Missing Page Type';
+    /**
+     * @var string
+     */
     protected $identifier = 'missing';
+    /**
+     * @var string
+     */
     protected $icon = 'fa-question-circle';
 
+    /**
+     * @var array
+     */
     protected $schema = [
         'name' => 'Name|textline|required|generates_slug'
     ];
 
+    /**
+     * @param $version
+     * @param array $data
+     * @return string
+     */
+    public function template($version, $data = [])
+    {
+        return 'coanda::missing_page_type';
+    }
 }
